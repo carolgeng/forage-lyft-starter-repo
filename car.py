8a723ab3727dfc1596a7c3fc952e8518cd4a5302
carolgeng <carolgeng27@gmail.com>
2023-05-27 18:24:07 -0500

    first commit
    Signed-off-by: carolgeng <carolgeng27@gmail.com>

--- car.py
+++ car.py
@@ -1,10 +1,15 @@
 from abc import ABC, abstractmethod
-
+from battery import Battery
+from engine import Engine
 
 class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+    def __init__(self, engine, battery):
+        # self.last_service_date = last_service_date
+        self.engine = engine
+        self.battery = battery
+
 
     @abstractmethod
     def needs_service(self):
-        pass
+        return self.engine.needs_service() or self.battery.needs_service()
+        # pass
